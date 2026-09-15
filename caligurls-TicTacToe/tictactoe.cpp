6e984ee93f3211dcf414986090b7efcc86c0d3c9
Artie129 <trnichol29@gmail.com>
2026-09-15 18:01:26 -0400

    CALI-8 code functions and meets all stated requirements.

--- caligurls-TicTacToe/tictactoe.cpp
+++ caligurls-TicTacToe/tictactoe.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
 #include <cstdlib>
 #include <ctime>
 #include <stdio.h>
@@ -12,16 +11,20 @@ bool returnToMainMenu = true;
 //bool for loop control and global int to let user make inputs
 
 void printBoard(int boardState[]);
-void changeBoard(int boardState[]);
+void playerChange(int boardState[]);
+bool checkWin(int boardState[]);
+void computerChange(int boardState[]);
 //function declaration for two functions used later
 
 int main()
 {
 	srand(time(NULL));
 	int randomNumber = (rand() % 9 + 1); //not implemented yet but generates a random number between 1 and 9 for the cpu to pick
-	int ticTacToe[9] = { 0,1,2,0,1,2,0,1,2 }; //edit this before selecting option 4 in the menu to test different board layouts.
+	int ticTacToe[9] = { 0,0,0,0,0,0,0,0,0 }; //edit this before selecting option 4 in the menu to test different board layouts.
 	bool gameRunning = true; 
 	bool playerChose = false; //both of these are misc bools used to control loops
+	bool playerWins = false;
+	bool CPUWins = false;
 
 	while(returnToMainMenu)
 	{
@@ -46,14 +49,43 @@ int main()
 		cout << "\nPlay selected.";
 		while (gameRunning)
 		{
+			cout << "Player turn\n";
+			cout << "\n\n";
+			playerChange(ticTacToe);
+			playerWins = checkWin(ticTacToe);
+			printBoard(ticTacToe);
+			if (playerWins)
+			{
+				cout << "\n\n\nPlayer wins!\n\n\n";
+				gameRunning = false;
+				break;
+			}
+			cout << "\n\n";
+			cout << "CPU turn\n";
+			computerChange(ticTacToe);
+			CPUWins = checkWin(ticTacToe);
+			printBoard(ticTacToe);
+			if (CPUWins)
+			{
+				cout << "\n\n\nCPU wins!\n\n\n";
+				gameRunning = false;
+				break;
+			}
 
-		}
+			}
+		
+		for (int i = 0; i < 9; i++)
+			{
+				ticTacToe[i] = 0;
+			}
+		//wipe the tic tac toe board so user can play again
+
 		break;
 
 	case 2:
 		cout << "\nInstructions:\n";
-		cout << "Get three of your symbol (X) in a horizontal, vertical, or diagonal row while blocking your opponent to win! Input your moves by selecting the corresponding grid space\n";
-		cout << " 1 2 3\n 4 5 6\n 7 8 9" << endl;
+		cout << "Get three of your symbol (X) in a horizontal, vertical, or diagonal row while blocking your opponent to win! Input your moves by selecting the cooresponding grid space\n";
+		cout << "1 2 3\n4 5 6\n7 8 9\n";
 		break;
 
 	case 3:
@@ -78,14 +110,77 @@ int main()
 	cout << "\nGoodbye!";
 }
 
-void changeBoard(int boardState[])
+bool checkWin(int boardState[])
+{
+	if ((boardState[0] != 0 && boardState[0] == boardState[1] && boardState[1] == boardState[2]) || (boardState[3] != 0 && boardState[3] == boardState[4] && boardState[4] == boardState[5]) ||	(boardState[6] != 0 && boardState[6] == boardState[7] && boardState[7] == boardState[8]))
+	{
+		//massive line above checks all three horizontals
+		return true;
+	}
+	else if ((boardState[0] != 0 && boardState[0] == boardState[3] && boardState[3] == boardState[6]) || (boardState[1] != 0 && boardState[1] == boardState[4] && boardState[4] == boardState[7]) || (boardState[2] != 0 && boardState[2] == boardState[5] && boardState[5] == boardState[8]))
+	{
+		//massive line above checks all three verticals
+		return true;
+	}
+	else if ((boardState[0] != 0 && boardState[0] == boardState[4] && boardState[4] == boardState[8]) || (boardState[2] != 0 && boardState[2] == boardState[4] && boardState[4] == boardState[6]))
+	{
+		//slightly less massive line checks diagonals;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	
+}
+
+void playerChange(int boardState[])
 {
 	bool playerChose = false;
-
-	cout << "Player, select the space ";
-	while (playerChose = false)
+	int choice = 777;
+	while (playerChose == false)
 	{
+		playerFailed:
+		cout << "Player, select move (1-9)\n";
+		cin >> choice;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(10000, '\n');
+			cout << "Input not valid\n";
+			goto playerFailed;
+		}
 
+		choice--;
+
+		if (boardState[choice] != 0)
+		{
+			cout << "invalid move. space is occupied. select again.";
+		}
+		else
+		{
+			boardState[choice] = 1;
+			playerChose = true;
+		}
+	}
+}
+
+void computerChange(int boardState[])
+{
+	bool CPUChose = false;
+	int choice = (rand() % 9 + 1);
+	
+	while (CPUChose == false)
+	{
+		if (boardState[choice] != 0)
+		{
+			choice = (rand() % 9 + 1);
+		}
+		else
+		{
+			boardState[choice] = 2;
+			CPUChose = true;
+		}
 	}
 }
 
