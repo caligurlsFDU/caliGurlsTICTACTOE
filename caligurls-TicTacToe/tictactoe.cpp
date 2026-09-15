0f8b1ba3ba819de188b8f0c3e74231849a821456
Artie129 <trnichol29@gmail.com>
2026-09-14 20:28:04 -0400

    CALI-8 added board array, random num generator, more menus and functions

--- caligurls-TicTacToe/tictactoe.cpp
+++ caligurls-TicTacToe/tictactoe.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
+#include <stdio.h>
 
 using namespace std;
 
 int userChoice;
 bool returnToMainMenu = true;
+//bool for loop control and global int to let user make inputs
+
+void printBoard(int boardState[]);
+void changeBoard(int boardState[]);
+//function declaration for two functions used later
 
 int main()
 {
+	srand(time(NULL));
+	int randomNumber = (rand() % 9 + 1); //not implemented yet but generates a random number between 1 and 9 for the cpu to pick
+	int ticTacToe[9] = { 0,1,2,0,1,2,0,1,2 }; //edit this before selecting option 4 in the menu to test different board layouts.
+	bool gameRunning = true; 
+	bool playerChose = false; //both of these are misc bools used to control loops
+
 	while(returnToMainMenu)
 	{
 	cout << "Welcome to Tic Tac Toe. Please select an option with the cooresponding num key\n";
@@ -21,11 +36,18 @@ int main()
 	switch (userChoice) //switch statement with all 4 (three but the fourth is temporary) options. Option 3 exits the loop with a goodbye message.
 	{
 	case 1:
+		gameRunning = true;
 		cout << "\nPlay selected.";
+		while (gameRunning)
+		{
+
+		}
 		break;
 
 	case 2:
-		cout << "\nInstructions.";
+		cout << "\nInstructions:\n";
+		cout << "Get three of your symbol (X) in a row while blocking your opponent to win! Input your moves by selecting the cooresponding grid space\n";
+		cout << "1 2 3\n 4 5 6\n 7 8 9";
 		break;
 
 	case 3:
@@ -34,11 +56,8 @@ int main()
 		break;
 
 	case 4:
-		cout << "\nDisplaying sample board:";
-		cout << "\n \n \n";
-		cout << "O X O\n";
-		cout << "X O X\n";
-		cout << "O X O\n";
+		cout << "\nDisplaying sample board:\n";
+		printBoard(ticTacToe);
 		break;
 
 	default:
@@ -49,4 +68,48 @@ int main()
 	}
 
 	cout << "\nGoodbye!";
+}
+
+void changeBoard(int boardState[])
+{
+	bool playerChose = false;
+
+	cout << "Player, select the space ";
+	while (playerChose = false)
+	{
+
+	}
+}
+
+void printBoard(int boardState[])
+{
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			switch(boardState[i * 3 + j])
+			{
+			case 0:
+				cout << "?";
+				break;
+
+			case 1:
+				cout << "X";
+				break;
+				
+			case 2:
+				cout << "O";
+				break;
+
+			default:
+				cout << "you shouldn't see this";
+			}
+
+			cout << " ";
+		}
+
+		cout << "\n";
+	}
+
 }
